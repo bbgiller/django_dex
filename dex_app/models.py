7967951fb3a0dbaf2f14dd5f951d97f2a1bf952b
ben <bgiller@berkeley.edu>
2023-03-06 11:21:13 -0500

    migration for user works and test user successfully saved

--- dex_app/models.py
+++ dex_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    firebase_id = models.CharField(max_length=100)
+    dexcom_id = models.CharField(max_length=100)
+    # Add more fields as needed
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
